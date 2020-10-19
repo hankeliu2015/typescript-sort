98e73963f632166d20cc5e190d781bbae6ce6ece
Hank Liu <hanke.liu@gmail.com>
2020-10-19 14:45:36 -0400

    add type gurard for union type

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,24 @@ class Sorter {
   //   this.collection = collection;
   // }
 
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
+
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j+1];
-          this.collection[j+1] = leftHand;
+        if (this.collection instanceof Array) {
+
+          if (this.collection[j] > this.collection[j]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j+1];
+            this.collection[j+1] = leftHand;
+          }
+        }
+
+        if(typeof this.collection === 'string') {
+        
         }
       }
     }
