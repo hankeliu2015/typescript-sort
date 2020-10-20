30f23e8fe7503cb6cfd24f7173a0a67957cb04b3
Hank Liu <hanke.liu@gmail.com>
2020-10-19 22:50:43 -0400

    update index

--- build/index.js
+++ build/index.js
@@ -1,5 +1,27 @@
 "use strict";
-console.log('Hi, there 123');
-var logSomething = function () {
-    console.log('something new');
-};
+var Sorter = /** @class */ (function () {
+    // collection: number[];
+    // constructor(collection: number[]) {
+    //   this.collection = collection;
+    // }
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                if (typeof this.collection === 'string') {
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
