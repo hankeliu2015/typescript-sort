97cd02c15c75d44276e1ed46eb68577bbaf2c979
Hank Liu <hanke.liu@gmail.com>
2020-10-19 22:49:30 -0400

    refactor sort algorithm, add sorter.js and numbersCollection.ts

--- build/index.js
+++ build/index.js
@@ -1,5 +1,37 @@
 "use strict";
-console.log('Hi, there 123');
-var logSomething = function () {
-    console.log('something new');
-};
+Object.defineProperty(exports, "__esModule", { value: true });
+var Sorter_1 = require("./Sorter");
+var NumbersCollection_1 = require("./NumbersCollection");
+var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 1000]);
+var sorter = new Sorter_1.Sorter(numbersCollection);
+sorter.sort();
+console.log(numbersCollection);
+// class Sorter {
+//   // collection: number[];
+//   // constructor(collection: number[]) {
+//   //   this.collection = collection;
+//   // }
+//
+//   constructor(public collection: number[] | string) {}
+//
+//   sort(): void {
+//     const { length } = this.collection;
+//
+//     for (let i = 0; i < length; i++) {
+//       for (let j = 0; j < length - i - 1; j++) {
+//         if (this.collection instanceof Array) {
+//
+//           if (this.collection[j] > this.collection[j]) {
+//             const leftHand = this.collection[j];
+//             this.collection[j] = this.collection[j+1];
+//             this.collection[j+1] = leftHand;
+//           }
+//         }
+//
+//         if(typeof this.collection === 'string') {
+//
+//         }
+//       }
+//     }
+//   }
+// }
